6c9fb511791136bf537f3b8eb2bc869b9748a82f
Karem <krmd91@gmail.com>
2018-06-05 17:50:19 -0500

    Base bien

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use PhpOffice\PhpWord\SimpleType\DocProtect;
+use PhpOffice\PhpWord\SimpleType\  DocProtect;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,32 +35,27 @@ class DefaultController extends Controller
         $empleo =  $query->getRepository('CoreBundle:TblEmpleado')->findOneBy(array('id'=>$ide));
         $est =  $query->getRepository('CoreBundle:TblEstacion')->findOneBy(array('id'=>$ids));
 
-        $fileName = 'C:\xampp56\htdocs\dc3\documentos\template.docx';
 
-        $phpTemplateObject = $this->get('phpword')->createTemplateObject($fileName);
 
-        $phpTemplateObject->setValue('Value1', $empleo->getNombre());
-        $phpTemplateObject->setValue('Value2', $empleo->getCurp());
-        $phpTemplateObject->setValue('Value3', $empleo->getPuesto());
-        $phpTemplateObject->setValue('Value4', $est->getRSocial());
-        $phpTemplateObject->setValue('Value5', $est->getRfc());
-        $phpTemplateObject->setValue('Value10', $est->getRTrabajador());
-        $phpTemplateObject->setValue('Value11', $est->getRPatronal());
-        $phpTemplateObject->setValue('Value6', $curso->getNombre());
-        $phpTemplateObject->setValue('Value7', $curso->getDuracion());
-        $phpTemplateObject->setValue('Value8', $curso->getFInicio()->format('d.m.Y'));
-        $phpTemplateObject->setValue('Value9', $curso->getFTermino()->format('d.m.Y'));
+        $templateWord = new TemplateProcessor('C:\xampp\htdocs\DC3b\documentos\template.docx');
 
-        $phpWordObject = $this->get('phpword')->getPhpWordObjFromTemplate($phpTemplateObject);
-        $phpWordObject->getProtection()->setEditing(DocProtect::NONE);
-        $phpWordObject->getCompatibility()->setOoxmlVersion(15);
-        $phpWordObject->setDefaultParagraphStyle(array('space' => array('before' => 0, 'after' => 0)));
-        $writer = $this->get('phpword')->createWriter($phpWordObject, 'Word2007');
-        $response = $this->get('phpword')->createStreamedResponse($writer);
-        $dispositionHeader = $response->headers->makeDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            'prueba.docx'
-        );
+
+
+        $templateWord->setValue('Value1', $empleo->getNombre());
+        $templateWord->setValue('Value2', $empleo->getCurp());
+        $templateWord->setValue('Value3', $empleo->getPuesto());
+        $templateWord->setValue('Value4', $est->getRSocial());
+        $templateWord->setValue('Value5', $est->getRfc());
+        $templateWord->setValue('Value10', $est->getRTrabajador());
+        $templateWord->setValue('Value11', $est->getRPatronal());
+        $templateWord->setValue('Value6', $curso->getNombre());
+        $templateWord->setValue('Value7', $curso->getDuracion());
+        $templateWord->setValue('Value8', $curso->getFInicio()->format('d.m.Y'));
+        $templateWord->setValue('Value9', $curso->getFTermino()->format('d.m.Y'));
+
+        $templateWord->saveAs('Prueba.docx');
+        header("Content-Disposition: attachment; filename=prueba.docx; charset=iso-8859-1");
+        echo file_get_contents('prueba.docx');
         $response->headers->set('Content-Type', 'application/msword');
         $response->headers->set('Pragma', 'public');
         $response->headers->set('Cache-Control', 'maxage=1');
@@ -68,7 +63,7 @@ class DefaultController extends Controller
 
         return $response;
 
-        /*$templateWord = new TemplateProcessor('C:\xampp\htdocs\DC3b\documentos\template.docx');
+        /*
         foreach ($empleo as $em) {
             $templateWord->setValue('Value1', $em->getNombre());
             $templateWord->setValue('Value2', $em->getCurp());
